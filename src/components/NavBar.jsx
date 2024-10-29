2d4d91f1025b576429c760a1338815b86f96adce
LuannaSantAnna <luannadcs@gmail.com>
2024-10-29 14:30:33 -0300

    feat: NavBar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,29 @@
+import "../css/NavBar.css";
+
+const NavBar = () => {
+  return (
+    <header>
+      <div className="logo">JGR | team store</div>
+      <nav>
+        <ul>
+          <li> <a href="http://globo.com">Legado Gibbs</a> </li>
+          <li> <a href="http://globo.com">Pilotos</a> </li>
+          <li> <a href="http://globo.com">Itens em promoção</a> </li>
+        </ul>
+      </nav>
+
+      <div className="navbar__icons">
+        <div> <form>
+          <input type="text" name="search" placeholder="Buscar" />
+          <button><i class="ri-search-line"></i></button>
+        </form> </div>
+        <div className="navbar__icons--item"> <a href="http://globo.com"><i class="ri-shopping-cart-line"></i></a> </div>
+        <div className="navbar__icons--item"> <a href="http://globo.com"><i class="ri-user-3-line"></i></a> </div>
+      </div>
+
+      <div className="menu__hamburguer"></div>
+    </header>
+  );
+}
+
+export default NavBar;
