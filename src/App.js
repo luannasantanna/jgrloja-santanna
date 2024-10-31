71e5b1185fad571a3019ce9bccdd04c6133550b8
LuannaSantAnna <luannadcs@gmail.com>
2024-10-31 11:17:51 -0300

    feat: TagPromoção / style: TagPromoção

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './index.css';
 import ItemListContainer from './components/ItemListContainer';
 import TagTitulo from './components/TagTitulo';
 import HeroHearder from './components/HeroHeader';
+import TagPromocao from './components/TagPromocao';
 
 function App() {
   return (
     <div className="App">
       <HeroHearder />
+      <TagPromocao text={'Promoção válida até 27/03/2024'} />
 
       <TagTitulo titulo={'Best Sellers'} subtitulo={'2024'} />
       <section className='container'>
---
+++ src/components/TagPromocao.jsx
@@ -0,0 +1,16 @@
+import '../css/TagPromocao.css';
+
+const TagPromocao = ( {text} ) => {
+
+    return (
+        <ul className='tag__promocao'>
+            <li>{text}</li>
+            <li>{text}</li>
+            <li>{text}</li>
+            <li>{text}</li>
+            <li>{text}</li>
+        </ul>
+    );
+}
+
+export default TagPromocao;
