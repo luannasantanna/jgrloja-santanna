edba001ea5276690bbe2b8dc50fdba8a3fedddc1
LuannaSantAnna <luannadcs@gmail.com>
2024-11-07 21:13:41 -0300

    atualização do ItemListContainer com promise/ instalação router

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,33 +1,6 @@
-import { useEffect, useState } from "react";
 import Item from "./Item";
-import BestSellers from "../service/BestSellers.mock";
 
-const ItemList = () => {
-    const [produtos, setProdutos] = useState();
-    const [loading, setLoading] = useState(true);
-
-    useEffect(() => {
-
-        const promesaProdutos = new Promise((resolve) => {
-            setTimeout(() => {
-                resolve(BestSellers);
-            }, 5000);
-        });
-
-        promesaProdutos
-            .then((response) => {
-                setProdutos(response);
-                setLoading(false);
-            })
-            .catch((error) => {
-                console.error('erro ao carregar produtos', error);
-            });
-    },); 
-
-    if (loading) {
-        return <div>Carregando ...</div>
-    };
-
+const ItemList = ({produtos}) => {
     return (
         < >
             {produtos.map((produto) => (<Item {...produto} />))};
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,13 +1,38 @@
+import { useEffect, useState } from "react";
 import TagTitulo from "./TagTitulo";
 import ItemList from "./ItemList";
+import BestSellers from "../service/BestSellers.mock";
 
 const ItemListContainer = () => {
-    return (
+    const [produtos, setProdutos] = useState();
+    const [loading, setLoading] = useState(true);
 
+    useEffect(() => {
+        const promesaProdutos = new Promise((resolve) => {
+            setTimeout(() => {
+                resolve(BestSellers);
+            }, 5000);
+        });
+
+        promesaProdutos
+            .then((response) => {
+                setProdutos(response);
+                setLoading(false);
+            })
+            .catch((error) => {
+                console.error('erro ao carregar produtos', error);
+            });
+    }, []);
+
+    if (loading) {
+        return <div>Carregando ...</div>
+    };
+
+    return (
         <section className='container'>
             <TagTitulo titulo={'Best Sellers'} subtitulo={'2024'} />
             <div className='container__content'>
-                <ItemList />
+                <ItemList produtos={produtos} />
             </div>
         </section>
 
